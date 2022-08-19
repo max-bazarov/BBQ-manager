e0334d92ecaf9b6e46ec0735ce0069376bbc1def
Yakov <yakov.varnaev@mail.ru>
2022-08-20 01:16:38 +0300

    migrations added

--- purchases/models.py
+++ purchases/models.py
@@ -9,7 +9,7 @@ class Purchase(models.Model):
     time = models.DateTimeField(auto_now_add=True)
     is_paid_by_card = models.BooleanField(default=False)
     procedures = models.ManyToManyField(
-        'procedures.Procedure',
+        'employees.MasterProcedure',
         related_name='procedures',
         through='PurchaseProcedure'
     )
@@ -21,14 +21,14 @@ class PurchaseProcedure(models.Model):
         db_table = 'purchase_procedure'
     
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    procedure = models.ForeignKey('procedures.MasterProcedure', on_delete=models.DO_NOTHING)
+    procedure = models.ForeignKey('employees.MasterProcedure', on_delete=models.DO_NOTHING)
 
 
 class UsedMaterial(models.Model):
 
     procedure = models.ForeignKey(
         PurchaseProcedure,
-        models=models.CASCADE,
+        on_delete=models.CASCADE,
         related_name='materials'
     )
     material = models.ForeignKey(
