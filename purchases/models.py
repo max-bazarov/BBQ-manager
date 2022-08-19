ad41d3196f1fb37c9adcd53dd760278c0c34eb0d
Yakov-Varnaev <73383566+Yakov-Varnaev@users.noreply.github.com>
2022-08-20 01:10:48 +0300

    used materials model (#15)

--- purchases/models.py
+++ purchases/models.py
@@ -6,6 +6,33 @@ class Purchase(models.Model):
     class Meta:
         db_table = 'procedure_purchases'
 
-    time = models.DateTimeField()
-    procedure = models.ForeignKey('employees.MasterProcedure', on_delete=models.DO_NOTHING)
+    time = models.DateTimeField(auto_now_add=True)
     is_paid_by_card = models.BooleanField(default=False)
+    procedures = models.ManyToManyField(
+        'procedures.Procedure',
+        related_name='procedures',
+        through='PurchaseProcedure'
+    )
+
+
+class PurchaseProcedure(models.Model):
+
+    class Meta:
+        db_table = 'purchase_procedure'
+    
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    procedure = models.ForeignKey('procedures.MasterProcedure', on_delete=models.DO_NOTHING)
+
+
+class UsedMaterial(models.Model):
+
+    procedure = models.ForeignKey(
+        PurchaseProcedure,
+        models=models.CASCADE,
+        related_name='materials'
+    )
+    material = models.ForeignKey(
+        'inventory.Material',
+        on_delete=models.DO_NOTHING,
+    )
+    amount = models.IntegerField(default=1)
