26e4c2c416684ede8d78b477ef2f75bfda7e5a40
Yakov-Varnaev <73383566+Yakov-Varnaev@users.noreply.github.com>
2022-08-20 17:58:11 +0300

    archive view mixin (#24)
    archive view mixin

---
+++ core/mixins/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+
+from core.services import ArchiveService
+
+
+class ArchiveViewMixin:
+    '''
+    This mixin provides `archive(self, request, *args, **kwargs)` method.
+    It can only be used with models, which has archived field.
+    '''
+
+    @action(detail=True, url_name='archive', methods=['put', 'patch'])
+    def archive(self, request, *args, **kwargs):
+        instance = self.get_object()
+        id = ArchiveService(instance).archive()
+        return Response({'id': id}, status=status.HTTP_200_OK)
