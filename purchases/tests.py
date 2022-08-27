2dc878cc0084b5dfde6a3bfae6758191bc281c6a
Max Bazarov <bazaroffm@gmail.com>
2022-08-27 22:19:28 +0300

    change test

--- purchases/tests.py
+++ purchases/tests.py
@@ -6,7 +6,10 @@ import pytest
 from django.test import TestCase
 from rest_framework.test import APITestCase
 
-from core.tests import BaseCRUDViewTest, NewBaseCreateTestMixin, NewBaseDestroyTestMixin, NewBaseUpdateTestMixin
+from core.tests import (BaseCRUDViewTest,
+                        NewBaseCreateTestMixin,
+                        NewBaseDestroyTestMixin,
+                        NewBaseUpdateTestMixin)
 from employees.models import Employee, MasterProcedure
 from inventory.models import Material, MaterialUnits
 from procedures.models import Procedure
@@ -16,10 +19,10 @@ from .serializers import UsedMaterialSerializer
 from .services import UsedMaterialService
 
 
-class TestUsedMaterialServiceNew(TestCase,
-                                 NewBaseCreateTestMixin,
-                                 NewBaseDestroyTestMixin,
-                                 NewBaseUpdateTestMixin):
+class TestUsedMaterialService(TestCase,
+                              NewBaseCreateTestMixin,
+                              NewBaseDestroyTestMixin,
+                              NewBaseUpdateTestMixin):
     model = UsedMaterial
     service = UsedMaterialService
 
@@ -27,86 +30,17 @@ class TestUsedMaterialServiceNew(TestCase,
     def setUpClass(cls) -> None:
         super().setUpClass()
         cls.instance = mixer.blend(cls.model)
-        cls.relations_queryset = cls.instance_with_relation.procedures.all()
         cls.data = {
-            'quantity': 1,
-            'material': cls.instance.id,
-            'procedure': cls.instance_with_relation.id
+            'amount': 1,
+            'material': mixer.blend(Material).id,
+            'procedure': mixer.blend(PurchaseProcedure).id,
         }
         cls.update_data = {
-            'quantity': 2,
-            'material': cls.instance.id,
-            'procedure': cls.instance_with_relation.id
+            'amount': 2,
+            'material': mixer.blend(Material).id,
+            'procedure': mixer.blend(PurchaseProcedure).id,
         }
-        cls.invalid_data = {}
-
-@pytest.mark.django_db
-class TestUsedMaterialService(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.model = UsedMaterial
-        cls.create_service = UsedMaterialService
-
-        cls.employee = Employee.objects.create(
-            first_name='name',
-            last_name='surname',
-            position='someone',
-            coefficient=1.11
-        )
-        cls.procedure_with_master = Procedure.objects.create(name='master procedure')
-        cls.master_procedure = MasterProcedure.objects.create(
-            procedure=cls.procedure_with_master,
-            employee=cls.employee,
-            price=Decimal(1),
-            coefficient=0.5,
-        )
-        cls.purchase = Purchase.objects.create(
-            time=datetime.now(),
-            is_paid_by_card=False,
-        )
-        cls.purchase_procedure = PurchaseProcedure.objects.create(
-            purchase=cls.purchase,
-            procedure=cls.master_procedure
-        )
-        cls.material = Material.objects.create(
-            name='Hair Color',
-            price=Decimal('1.11'),
-            unit=MaterialUnits.GRAMMS.value
-        )
-        cls.data = {
-            'procedure': cls.purchase_procedure,
-            'material': cls.material,
-            'amount': 2
-        }
-        cls.instance = UsedMaterial.objects.create(
-            procedure=cls.purchase_procedure,
-            material=cls.material,
-            amount=1
-        )
-
-    def test_create(self):
-        count = self.model.objects.count()
-        instance = self.create_service(**self.data).create()
-
-        assert count + 1 == self.model.objects.count(), (
-            f'{self.create_service.__class__.__name__} does not create instance'
-        )
-        assert isinstance(instance, self.model), (
-            f'{self.create_service.__class__.__name__} create method does not return instance'
-        )
-        for k, v in self.data.items():
-            assert v == getattr(instance, k)
-
-    def test_destroy(self):
-        count = self.model.objects.count()
-
-        self.create_service(self.instance).destroy()
-
-        assert self.model.objects.count() == count - 1, (
-            f'{self.create_service.__class__.__name__} does not delete instance after destroy.'
-        )
+        cls.invalid_data = {'material': 'test'}
 
 
 @pytest.mark.django_db
