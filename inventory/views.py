a0441996d2b4bb32e2b5f21385887e2b23d92b2a
Max Bazarov <bazaroffm@gmail.com>
2022-08-29 00:55:01 +0300

    delete deprecated

--- inventory/views.py
+++ inventory/views.py
@@ -3,22 +3,20 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.mixins.views import ArchiveViewMixin
-from core.services import ArchiveService
 from inventory.models import Material
 from inventory.serializers import MaterialSerializer
-from inventory.services import MaterialDestroyService
+
+from .services import MaterialService
 
 
-class MaterialViewSet(ModelViewSet,
-                      ArchiveViewMixin):
-    queryset = Material.objects.filter()
+class MaterialViewSet(ModelViewSet, ArchiveViewMixin):
+    queryset = Material.objects.all()
     serializer_class = MaterialSerializer
 
     def update(self, request, *args, **kwargs):
         try:
             instance = self.get_object()
-            updated_instance = ArchiveService(instance,
-                                              self.serializer_class).update(**request.data)
+            updated_instance = MaterialService(instance, data=request.data, **kwargs).update()
             return Response(MaterialSerializer(updated_instance).data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -28,7 +26,7 @@ class MaterialViewSet(ModelViewSet,
 
     def destroy(self, request, *args, **kwargs):
         try:
-            MaterialDestroyService(self.get_object()).destroy()
+            MaterialService(instance=self.get_object()).destroy()
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_204_NO_CONTENT)
