238557e3de7a162f64ab3863f71aa0324185a8da
Alexandr Masnoy <icecubeloc@gmail.com>
2022-08-31 15:00:03 +0300

    added admin for inventory and fix

--- inventory/views.py
+++ inventory/views.py
@@ -34,9 +34,6 @@ class MaterialViewSet(ModelViewSet):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-    def partial_update(self, request, *args, **kwargs):
-        return self.update(request, partial=True, *args, **kwargs)
-
     def destroy(self, request, *args, **kwargs):
         try:
             MaterialService(instance=self.get_object()).destroy()
@@ -63,9 +60,6 @@ class ProductMaterialViewSet(ModelViewSet):
                             status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, *args, **kwargs):
-        return self.update(request, partial=True, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         try:
